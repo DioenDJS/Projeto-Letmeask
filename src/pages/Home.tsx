91af42ad37831242da75fce76d38f3d5b9f81e8d
DioenDJS <negociospacheti@gmail.com>
2021-07-12 01:51:41 -0300

    subindo autenticacao roon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { auth, firebase } from '../services/firebase';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -10,12 +9,19 @@ import { Button } from '../components/Button';
 
 
 import '../styles/auth.scss';
+import { AuthContext } from '../Router';
 
 export function Home() {
 
     const history = useHistory();
+    const {user, signInWithGoogle } = useContext(AuthContext)
     
     function handleCreateRoom(){
+
+        if(!user){
+            signInWithGoogle();
+        }
+
         history.push('/rooms/new');
     }
 
